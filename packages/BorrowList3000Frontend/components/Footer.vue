b6270997db5aec6593f1522b64ffda951fdd2b98
Finn-Thorben Sell <dev@finn-thorben.me>
2021-07-26 09:47:44 +0200

    simplify default layout and configure footer

---
+++ packages/BorrowList3000Frontend/components/Footer.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <v-footer
+      :absolute="true"
+      app
+    >
+      <v-row justify="center" no-gutters>
+        <div
+          v-for="item of items"
+          :key="item.text"
+        >
+          <a v-if="item.link" class="no-style" :href="item.link">
+            <v-btn text rounded>
+              <v-icon>{{ item.icon }}</v-icon>
+              <span>{{ item.text }}</span>
+            </v-btn>
+          </a>
+        </div>
+      </v-row>
+    </v-footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+  data () {
+    return {
+      items: [
+        {
+          icon: 'mdi-copyright',
+          text: new Date().getFullYear(),
+          link: 'https://github.com/ftsell/BorrowList3000/blob/main/LICENSE'
+        },
+        {
+          icon: 'mdi-github',
+          text: 'GitHub',
+          link: 'https://github.com/ftsell/borrowList3000/'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+a.no-style,
+a.no-style:link,
+a.no-style:visited,
+a.no-style:hover,
+a.no-style:active {
+  color: inherit;
+  text-decoration: inherit;
+  font-weight: inherit;
+}
+</style>
