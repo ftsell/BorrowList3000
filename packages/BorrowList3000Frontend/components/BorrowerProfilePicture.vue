6e3be6843faa5e517195d5c2985acbf43f64dcec
Finn-Thorben Sell <dev@finn-thorben.me>
2021-07-28 17:46:53 +0200

    add component for short display of a borrower

---
+++ packages/BorrowList3000Frontend/components/BorrowerProfilePicture.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-avatar
+    color="primary"
+    size="56"
+  />
+</template>
+
+<script>
+export default {
+  name: "BorrowerProfilePicture"
+};
+</script>
---
+++ packages/BorrowList3000Frontend/components/BorrowerShorty.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-col class="d-flex align-center">
+    <borrower-profile-picture class="mr-4" />
+
+    <div class="mr-8">
+      <span class="text-capitalize text-h3 font-weight-light">{{ borrower.name }}</span>
+    </div>
+
+    <div>
+      <span v-if="borrower.borrowedItems.length > 0" class="font-italic">{{ borrower.borrowedItems.length }} items borrowed</span>
+      <span v-else class="font-italic">Nothing borrowed</span>
+    </div>
+  </v-col>
+</template>
+
+<script>
+import BorrowerProfilePicture from "~/components/BorrowerProfilePicture";
+
+export default {
+  name: "BorrowerShorty",
+  components: { BorrowerProfilePicture },
+  props: ["borrower"]
+};
+</script>
