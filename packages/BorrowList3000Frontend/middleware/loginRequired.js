007844d6302be1670cc3a1285602feaffe9fc797
Finn-Thorben Sell <dev@finn-thorben.me>
2021-07-28 22:16:30 +0200

    implement loginRequired middleware

---
+++ packages/BorrowList3000Frontend/middleware/loginRequired.js
@@ -0,0 +1,18 @@
+import gql from "graphql-tag";
+
+export default async function(context) {
+    if (process.server) {
+        if (context.req.session.loggedIn !== true) {
+            context.redirect("/");
+        }
+    } else {
+        const loggedIn = (await context.app.apolloProvider.defaultClient.query({
+            query: gql`{
+                loggedIn
+            }`
+        })).data.loggedIn;
+        if (loggedIn !== true) {
+            context.redirect("/")
+        }
+    }
+}
