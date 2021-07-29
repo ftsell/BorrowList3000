606784678a9e1ec98d18f52b056f67a8baa6da9a
Finn-Thorben Sell <dev@finn-thorben.me>
2021-07-29 12:45:41 +0200

    implement authentication view

---
+++ packages/BorrowList3000Frontend/pages/auth.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row align="center" justify="center">
+    <v-col cols="6" offset="2">
+      <v-card>
+        <auth-form @onLoggedIn="onLoggedIn" />
+
+        <v-alert v-if="alert" :type="alert.type" text class="mt-2">
+          {{ alert.message }}
+        </v-alert>
+      </v-card>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+import AuthForm from "~/components/forms/AuthForm";
+
+export default {
+  name: "Auth",
+  components: { AuthForm },
+  data: () => ({
+    alert: null,
+  }),
+  methods: {
+    onLoggedIn(result) {
+      if (result.success) {
+        this.alert = null
+        this.$router.push({path: "/app"})
+      } else {
+        this.alert = {
+          type: "error",
+          message: result.message
+        }
+      }
+    }
+  }
+};
+</script>
