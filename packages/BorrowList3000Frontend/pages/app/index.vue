06049106cb5386b459d1d8f47c3ef52e9c4a9dd2
Finn-Thorben Sell <dev@finn-thorben.me>
2021-07-29 13:20:10 +0200

    add simple app dashboard that displays all borrowers

--- packages/BorrowList3000Frontend/pages/app/index.vue
+++ packages/BorrowList3000Frontend/pages/app/index.vue
@@ -1,37 +1,45 @@
 <template>
-  <div>
-    <div>{{ user }}</div>
-    <v-btn @click="$fetch">Reload</v-btn>
-  </div>
+  <v-row align="center" class="mx-16">
+    <v-col cols="4">
+      <create-borrower-form @onBorrowerCreated="$fetch" />
+    </v-col>
+    <v-col v-for="borrower of user.borrowers" :key="borrower.name" cols="4">
+      <borrower-shorty :borrower="borrower" />
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 import gql from "graphql-tag";
+import BorrowerShorty from "~/components/BorrowerShorty";
+import CreateBorrowerForm from "~/components/forms/CreateBorrowerForm";
 
 export default {
   name: "AppIndex",
+  components: { CreateBorrowerForm, BorrowerShorty },
   middleware: ["loginRequired"],
   data: () => ({
     user: null
   }),
   async fetch() {
     if (process.server) {
-      this.user = await this.$nuxt.context.$db.users.getUserByUsername(this.$nuxt.context.req.session.username)
+      this.user = await this.$nuxt.context.$db.users.getUserByUsername(this.$nuxt.context.req.session.username, true);
     } else {
-      this.user = (await this.$apollo.query({query: gql`{
-        me {
-          username
-          borrowers {
-            name
-            borrowedItems {
-              specifier
-              description
-              dateBorrowed
+      this.user = (await this.$apollo.query({
+        query: gql`{
+          me {
+            username
+            borrowers {
+              name
+              borrowedItems {
+                specifier
+              }
             }
           }
-        }
-      }`})).data.me
+        }`,
+        fetchPolicy: "no-cache"
+      })).data.me;
     }
-  },
+  }
 };
 </script>
