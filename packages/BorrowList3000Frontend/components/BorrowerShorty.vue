6b53079dc92ffb6d8cbbf6df67d2876b571875b8
Finn-Thorben Sell <dev@finn-thorben.me>
2021-07-29 14:36:34 +0200

    modify BorrowerShorty to add delete and open buttons and display it in a card

--- packages/BorrowList3000Frontend/components/BorrowerShorty.vue
+++ packages/BorrowList3000Frontend/components/BorrowerShorty.vue
@@ -1,16 +1,32 @@
 <template>
-  <v-col class="d-flex align-center">
-    <borrower-profile-picture class="mr-4" />
+  <v-card shaped outlined>
+    <v-card-text>
+      <div class="d-flex align-center">
+        <borrower-profile-picture class="mr-4" />
 
-    <div class="mr-8">
-      <span class="text-capitalize text-h3 font-weight-light">{{ borrower.name }}</span>
-    </div>
+        <div class="mr-8">
+          <span class="text-capitalize text-h3 font-weight-light">{{ borrower.name }}</span>
+        </div>
 
-    <div>
-      <span v-if="borrower.borrowedItems.length > 0" class="font-italic">{{ borrower.borrowedItems.length }} items borrowed</span>
-      <span v-else class="font-italic">Nothing borrowed</span>
-    </div>
-  </v-col>
+        <div>
+          <span v-if="borrower.borrowedItems.length > 0" class="font-italic">{{ borrower.borrowedItems.length }} items borrowed</span>
+          <span v-else class="font-italic">Nothing borrowed</span>
+        </div>
+      </div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-spacer />
+      <nuxt-link :to="`/app/${borrower.name}`">
+        <v-btn icon>
+          <v-icon>mdi-open-in-app</v-icon>
+        </v-btn>
+      </nuxt-link>
+      <v-btn icon>
+        <v-icon @click="onDelete">mdi-delete</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
@@ -19,6 +35,12 @@ import BorrowerProfilePicture from "~/components/BorrowerProfilePicture";
 export default {
   name: "BorrowerShorty",
   components: { BorrowerProfilePicture },
-  props: ["borrower"]
+  props: ["borrower"],
+  methods: {
+    onDelete() {
+      // TODO Implement borrower deletion
+      throw new Error("not yet implemented")
+    }
+  }
 };
 </script>
