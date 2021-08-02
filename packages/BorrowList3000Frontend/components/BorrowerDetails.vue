f581936714a8b1e67fc25a20da97692f5694eb00
Finn-Thorben Sell <dev@finn-thorben.me>
2021-08-02 21:34:56 +0200

    add component which displays a borrower in detail

---
+++ packages/BorrowList3000Frontend/components/BorrowerDetails.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <create-borrowed-item-form :borrower-name="borrower.name" />
+
+    <borrowed-item-shorty
+      v-for="item of borrower.borrowedItems"
+      :key="item.id"
+      :item="item"
+    />
+  </div>
+</template>
+
+<script>
+import BorrowedItemShorty from "~/components/BorrowedItemShorty";
+import CreateBorrowedItemForm from "~/components/forms/CreateBorrowedItemForm";
+
+export default {
+  name: "BorrowerDetails",
+  components: { CreateBorrowedItemForm, BorrowedItemShorty },
+  props: {
+    borrower: Object,
+    default: null
+  }
+};
+</script>
