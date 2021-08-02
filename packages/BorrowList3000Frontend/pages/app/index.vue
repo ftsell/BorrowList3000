29f3302fe6923103a607f441b0a0dc9b529ff9cc
Finn-Thorben Sell <dev@finn-thorben.me>
2021-08-02 20:27:53 +0200

    show popup in which borrower details can be displayed

--- packages/BorrowList3000Frontend/pages/app/index.vue
+++ packages/BorrowList3000Frontend/pages/app/index.vue
@@ -1,15 +1,33 @@
 <template>
-  <v-row align="center" class="mx-16" v-if="user != null">
-    <v-col cols="4">
-      <create-borrower-form @onBorrowerCreated="$fetch" />
-    </v-col>
-    <v-col v-for="borrower of user.borrowers" :key="borrower.name" cols="4">
-      <borrower-shorty :borrower="borrower" />
-    </v-col>
-  </v-row>
+  <div v-if="user != null">
+    <!-- Normal page content -->
+    <v-row align="center" class="mx-16">
+      <v-col cols="4">
+        <create-borrower-form @onBorrowerCreated="$fetch" />
+      </v-col>
+      <v-col v-for="borrower of user.borrowers" :key="borrower.name" cols="4">
+        <borrower-shorty :borrower="borrower" />
+      </v-col>
+    </v-row>
+
+    <!-- Show Borrower dialog -->
+    <v-dialog
+      :value="showBorrower != null"
+      @input="(value) => showBorrower = value"
+      max-width="60vw"
+    >
+      <v-card v-if="showBorrower != null">
+        <v-card-title>{{ showBorrower.name }}</v-card-title>
+        <v-card-text>
+          Test
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script>
+import { omit } from "lodash";
 import gql from "graphql-tag";
 import BorrowerShorty from "~/components/BorrowerShorty";
 import CreateBorrowerForm from "~/components/forms/CreateBorrowerForm";
@@ -21,9 +39,26 @@ export default {
   data: () => ({
     user: null
   }),
+  computed: {
+    showBorrower: {
+      get() {
+        if (this.$route.query.borrower != null || this.user != null) {
+          return this.user.borrowers.find(b => b.name === this.$route.query.borrower);
+        }
+        return null;
+      },
+      set(value) {
+        if (!value) {
+          this.$router.push({
+            query: omit(this.$route.query, ["borrower"])
+          });
+        }
+      }
+    }
+  },
   async fetch() {
     if (process.server) {
-      this.user = await this.$nuxt.context.$db.users.getUserByUsername(this.$nuxt.context.req.session.username, true);
+      this.user = (await this.$nuxt.context.$db.users.getUserByUsername(this.$nuxt.context.req.session.username, true)).toJSON();
     } else {
       this.user = (await this.$apollo.query({
         query: gql`{
