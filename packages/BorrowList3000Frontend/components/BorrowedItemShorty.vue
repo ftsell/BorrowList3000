56ac9da417f1d386856b1bd813c8b09c4c6b280f
Finn-Thorben Sell <dev@finn-thorben.me>
2021-08-07 11:58:33 +0200

    add button to return a borrowed item (not yet implemented)

--- packages/BorrowList3000Frontend/components/BorrowedItemShorty.vue
+++ packages/BorrowList3000Frontend/components/BorrowedItemShorty.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="d-flex flex-column align-start">
-    <div>
-      <span class="text-h5 font-weight-light">{{ item.specifier }}</span>
+  <div class='d-flex flex-row'>
+    <!-- Actions -->
+    <div class='d-flex flex-column align-start mr-2'>
+      <v-btn @click='onReturnClicked' icon>
+        <v-icon>mdi-keyboard-return</v-icon>
+      </v-btn>
     </div>
 
-    <div v-if="item.description != null">
-      <span class="font-italic">{{ item.description }}</span>
+    <!-- Data display -->
+    <div class="d-flex flex-column align-start">
+      <div>
+        <span class="text-h5 font-weight-light">{{ item.specifier }}</span>
+      </div>
+
+      <div v-if="item.description != null">
+        <span class="font-italic">{{ item.description }}</span>
+      </div>
     </div>
   </div>
 </template>
@@ -17,6 +27,11 @@ export default {
     item: {
       type: Object,
       required: true
+    }
+  },
+  methods: {
+    onReturnClicked() {
+      throw new Error("not yet implemented")
     }
   }
 };
