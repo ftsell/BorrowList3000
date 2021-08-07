0cc734b6a4325ec9008f84d9fcf17d0e4c1d69a4
Finn-Thorben Sell <dev@finn-thorben.me>
2021-08-07 09:12:31 +0200

    sort borrowers based on number of borrowed items

--- packages/BorrowList3000Frontend/store/index.js
+++ packages/BorrowList3000Frontend/store/index.js
@@ -4,13 +4,19 @@ export const state = () => ({
     user: null,
 });
 
+function sortUsers(state) {
+    state.user.borrowers = state.user.borrowers.sort((a, b) => b.borrowedItems.length - a.borrowedItems.length)
+}
+
 export const mutations = {
     updateUser(state, newUser) {
         state.user = newUser;
+        sortUsers(state)
     },
     addBorrower(state, newBorrower) {
         assert(state.user != null);
         state.user.borrowers.push(newBorrower);
+        sortUsers(state)
     },
     addBorrowedItem(state, { borrowerName, newItem }) {
         assert(state.user != null);
@@ -21,6 +27,7 @@ export const mutations = {
         assert(borrower != null);
 
         borrower.borrowedItems.push(newItem);
+        sortUsers(state)
     },
 };
 
