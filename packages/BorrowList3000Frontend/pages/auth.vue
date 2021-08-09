6e74cccd1cf0e8ea144af960141f316f93f45775
Finn-Thorben Sell <dev@finn-thorben.me>
2021-08-09 12:13:08 +0200

    remove obsolete alert display from auth page
    Alerts can now be displayed in a more general fashion which is already implemented in AuthForm.vue

--- packages/BorrowList3000Frontend/pages/auth.vue
+++ packages/BorrowList3000Frontend/pages/auth.vue
@@ -3,10 +3,6 @@
     <v-col cols="12">
       <v-card>
         <auth-form @onLoggedIn="onLoggedIn" />
-
-        <v-alert v-if="alert" :type="alert.type" text class="mt-2">
-          {{ alert.message }}
-        </v-alert>
       </v-card>
     </v-col>
   </v-row>
@@ -18,19 +14,10 @@ import AuthForm from "~/components/forms/AuthForm";
 export default {
   name: "Auth",
   components: { AuthForm },
-  data: () => ({
-    alert: null,
-  }),
   methods: {
     onLoggedIn(result) {
-      if (result.success) {
-        this.alert = null
+      if (result.success === true) {
         this.$router.push({path: "/app"})
-      } else {
-        this.alert = {
-          type: "error",
-          message: result.message
-        }
       }
     }
   }
