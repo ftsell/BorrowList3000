dafff1a903255dce9a0c59a04d7c1600be03d510
Finn-Thorben Sell <dev@finn-thorben.me>
2021-08-17 12:07:09 +0200

    fix database reset to only reset application data tables

--- packages/BorrowList3000Backend/src/api/devController.js
+++ packages/BorrowList3000Backend/src/api/devController.js
@@ -1,11 +1,14 @@
-import { sequelize } from "../db";
 import consola from "consola";
+import { UserModel } from "../db/models";
+import { SessionModel } from "../db/sessions";
 
 export async function resetDb() {
     if (process.env.BL_ALLOW_DB_RESET === "true") {
-        consola.warn("Truncating all database tables")
-        await sequelize.truncate()
-        return true
+        consola.warn("Truncating all database tables");
+
+        await Promise.all([UserModel.truncate(), SessionModel.truncate()]);
+
+        return true;
     }
-    return false
+    return false;
 }
