bf73bf755b652d2e3609be7db9d63f1cff05183b
Finn-Thorben Sell <dev@finn-thorben.me>
2021-08-27 17:41:54 +0200

    remove csrf middleware because it is not needed

--- src/borrowlist3000/settings.py
+++ src/borrowlist3000/settings.py
@@ -41,7 +41,6 @@ class Base(Configuration):
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
-        'django.middleware.csrf.CsrfViewMiddleware',
         'django.contrib.auth.middleware.AuthenticationMiddleware',
         'django.contrib.messages.middleware.MessageMiddleware',
         'django.middleware.clickjacking.XFrameOptionsMiddleware',
