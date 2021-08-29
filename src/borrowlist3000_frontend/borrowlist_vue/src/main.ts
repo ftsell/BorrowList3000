53025e54418199b7fe61e00a808042fa15a0479b
Finn-Thorben Sell <dev@finn-thorben.me>
2021-08-29 16:48:23 +0200

    gui: setup apollo and vuetify

--- src/borrowlist3000_frontend/borrowlist_vue/src/main.ts
+++ src/borrowlist3000_frontend/borrowlist_vue/src/main.ts
@@ -1,10 +1,14 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import { apolloProvider } from "./vue-apollo";
+import { vuetify } from "./vuetify";
 
 Vue.config.productionTip = false;
 
 new Vue({
     router,
+    apolloProvider,
+    vuetify,
     render: (h) => h(App),
 }).$mount("#app");
