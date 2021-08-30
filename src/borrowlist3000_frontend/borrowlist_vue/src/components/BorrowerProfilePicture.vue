f83161425261d659e33eaaa72268cee72566a798
Finn-Thorben Sell <dev@finn-thorben.me>
2021-08-30 10:41:30 +0200

    gui: add BorrowerProfilePicture from old nuxt project

---
+++ src/borrowlist3000_frontend/borrowlist_vue/src/components/BorrowerProfilePicture.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-avatar color="primary" size="56" />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class BorrowerProfilePicture extends Vue {}
+</script>
