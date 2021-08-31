f49132ab0fc3223773920e09b8ef0d4109fc63ca
Finn-Thorben Sell <dev@finn-thorben.me>
2021-08-31 15:34:04 +0200

    add automatic token secret generation to docker start script

--- .docker/prestart.sh
+++ .docker/prestart.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/sh
 set -e
 
+if [ -z "$BL_TOKEN_SECRET" ]; then
+    echo "Generating a new token secret since none is defined."
+    export DJANGO_CONFIGURATION=Dev
+    ./manage.py gen_token_secret
+    exit 1
+fi
+
 ./manage.py migrate
