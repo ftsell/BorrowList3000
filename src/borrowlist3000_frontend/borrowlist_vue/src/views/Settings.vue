f54387dea54fae3e9cab8254ec18968bfce176c1
Finn-Thorben Sell <dev@finn-thorben.me>
2021-09-08 21:07:45 +0200

    gui: extract settings form into own component

--- src/borrowlist3000_frontend/borrowlist_vue/src/views/Settings.vue
+++ src/borrowlist3000_frontend/borrowlist_vue/src/views/Settings.vue
@@ -1,98 +1,29 @@
 <template>
   <app-layout>
-    <div v-if="user != null" class="mx-16">
-      <v-form ref="form" :disabled="!editable" @submit.prevent="onFormSubmit">
-        <v-container>
-          <!-- Form Fields -->
-          <v-row>
-            <v-text-field v-model="user.username" label="Username" disabled />
-          </v-row>
-          <v-row>
-            <v-text-field
-              v-model="user.password"
-              label="Password"
-              type="password"
-            />
-          </v-row>
-          <v-row v-if="appConfig.emailEnabled">
-            <v-text-field v-model="user.email" label="EMail address" />
-          </v-row>
+    <user-edit-form />
 
-          <!-- Form Actions -->
-          <v-row class="d-flex flex-row">
-            <v-btn v-if="editable" class="mr-4" color="primary" type="submit"
-              >Save
-            </v-btn>
-            <v-btn :outlined="editable" @click="editable = !editable">
-              <span v-if="!editable">Edit</span>
-              <span v-else>Cancel</span>
-            </v-btn>
-          </v-row>
-        </v-container>
-      </v-form>
+    <v-spacer class="my-8" />
 
-      <v-spacer class="my-8" />
-
-      <v-container>
-        <v-row>
-          <v-btn color="red" outlined @click="onDeleteClicked"
-            >Delete Account
-          </v-btn>
-        </v-row>
-      </v-container>
-    </div>
+    <v-container>
+      <v-row>
+        <v-btn color="red" outlined @click="onDeleteClicked"
+          >Delete Account
+        </v-btn>
+      </v-row>
+    </v-container>
   </app-layout>
 </template>
 
 <script lang="ts">
-import { Component, Vue, InjectReactive } from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 import gql from "graphql-tag";
 import AppLayout from "@/layouts/app.vue";
+import UserEditForm from "@/components/forms/UserEditForm.vue";
 
 @Component({
-  components: { AppLayout },
-  apollo: {
-    user: gql`
-      query {
-        user: me {
-          id
-          username
-          email
-        }
-      }
-    `,
-  },
+  components: { AppLayout, UserEditForm },
 })
 export default class Settings extends Vue {
-  @InjectReactive() appConfig!: any;
-  user: any | null = null;
-  editable = false;
-
-  get formElement(): any {
-    return this.$refs.form;
-  }
-
-  async onFormSubmit(): Promise<void> {
-    if (this.formElement.validate()) {
-      await this.$apollo.mutate({
-        mutation: gql`
-          mutation ($email: String, $password: String) {
-            alterUser(user: { email: $email, password: $password }) {
-              user {
-                id
-                username
-                email
-              }
-            }
-          }
-        `,
-        variables: this.user,
-      });
-
-      this.editable = false;
-    }
-  }
-
   async onDeleteClicked(): Promise<void> {
     if (
       confirm(
