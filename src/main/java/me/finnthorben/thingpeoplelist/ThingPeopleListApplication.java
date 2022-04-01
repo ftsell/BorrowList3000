45c962924e5e7a545d32ac16717ee56ec7726a1a
Finn-Thorben Sell <dev@finn-thorben.me>
2022-04-01 14:11:30 +0200

    add spring-initializr based project

---
+++ src/main/java/me/finnthorben/thingpeoplelist/ThingPeopleListApplication.java
@@ -0,0 +1,13 @@
+package me.finnthorben.thingpeoplelist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThingPeopleListApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThingPeopleListApplication.class, args);
+    }
+
+}
