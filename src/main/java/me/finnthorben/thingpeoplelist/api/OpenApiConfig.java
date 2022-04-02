39859c45d0618667b25e72fc21be552f846510bf
Finn-Thorben Sell <dev@finn-thorben.me>
2022-04-02 21:55:29 +0200

    add customized OpenApi configuration

---
+++ src/main/java/me/finnthorben/thingpeoplelist/api/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package me.finnthorben.thingpeoplelist.api;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+    @Bean
+    public OpenAPI thingPeopleListOpenApiConfig() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Thing-People-List Api")
+                        .description("Public API of the Thing-People-List Service.\n" +
+                                "Thing-People-List is a Web-Service to keep track of which people did what i.e. who " +
+                                "borrowed what from you.")
+                        .contact(new Contact()
+                                .name("Finn-Thorben Sell")
+                                .email("dev@finn-thorben.me"))
+                        .license(new License().name("MIT"))
+                );
+    }
+}
