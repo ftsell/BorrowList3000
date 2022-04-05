1f1c3632ea533dd6f5b254fc9631f27fe059daca
Finn-Thorben Sell <dev@finn-thorben.me>
2022-04-05 09:23:08 +0200

    remove NoSuchThingException definition

--- src/main/java/me/finnthorben/thingpeoplelist/things/IThingService.java
+++ src/main/java/me/finnthorben/thingpeoplelist/things/IThingService.java
@@ -3,16 +3,9 @@ package me.finnthorben.thingpeoplelist.things;
 import me.finnthorben.thingpeoplelist.lists.ThingList;
 import me.finnthorben.thingpeoplelist.people.Person;
 
-import java.util.NoSuchElementException;
 import java.util.Set;
 
 public interface IThingService {
-    class NoSuchThingException extends NoSuchElementException {
-        public NoSuchThingException(String thingName, ThingList list) {
-            super("There is no thing " + thingName + " in list " + list.getName());
-        }
-    }
-
     /**
      * Create a new persisted thing that is entered in the given list and associated with the given person
      */
