7f4f066f36896e85dc428a38f261821b5367a8a3
Finn-Thorben Sell <dev@finn-thorben.me>
2022-04-05 23:31:51 +0200

    WIP

---
+++ src/frontend/src/components/componentLibrary/Card.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="card">
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+.card {
+  padding: 8px;
+  border-radius: 4px;
+  box-shadow: 0 0 16px 8px rgba(70, 137, 102, 0.1),
+    0.5px 0.5px 4px 1px var(--color-secondary-alt);
+}
+</style>
---
+++ src/frontend/src/views/AuthView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Card from "@/components/componentLibrary/Card.vue";
+</script>
+
+<template>
+  <h1>Hi</h1>
+  <Card class="x">
+    <h2>World</h2>
+  </Card>
+</template>
+
+<style scoped>
+.x {
+  width: 80%;
+}
+</style>
