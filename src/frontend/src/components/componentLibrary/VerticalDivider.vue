691d2f399cb124fcd07e2652104d94ad60865d5c
Finn-Thorben Sell <dev@finn-thorben.me>
2022-04-06 20:14:49 +0200

    frontend: implement vertical divider component

---
+++ src/frontend/src/components/componentLibrary/VerticalDivider.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="vertical-divider" />
+</template>
+
+<style scoped>
+.vertical-divider {
+  margin-right: 16px;
+  padding: 1px;
+  border-radius: 2px;
+  background: var(--color-secondary);
+  filter: opacity(30%);
+}
+</style>
