40b0fe3f1e2e3b55f4953676e8ceddcfe3ace3f3
Finn-Thorben Sell <dev@finn-thorben.me>
2022-04-08 11:37:11 +0200

    frontend: implement custom button component

---
+++ src/frontend/src/components/componentLibrary/CustomButton.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { computed } from "vue";
+
+const props = defineProps<{
+  text: string;
+  type?: string;
+  outlined?: boolean;
+}>();
+defineEmits<{
+  (e: "click"): void;
+}>();
+const cssClasses = computed(() => {
+  return {
+    outlined: Boolean(props.outlined),
+  };
+});
+</script>
+
+<template>
+  <button :type="type" @click="$emit('click')" :class="cssClasses">
+    {{ text }}
+  </button>
+</template>
+
+<style scoped>
+button {
+  padding: 0.4em 0.8em;
+  border-radius: 4px;
+  font-size: large;
+}
+
+button:focus {
+  outline: 0.15em solid var(--color-primary);
+}
+
+button:not(.outlined) {
+  background-color: var(--color-primary);
+  color: var(--color-background);
+  border: solid 1px var(--color-background);
+}
+
+button.outlined {
+  border: solid 2px var(--color-primary);
+  color: var(--color-primary);
+}
+</style>
