4dc5cdaff00783f588cb23ffd8ba8e87b62d9bf4
Finn-Thorben Sell <dev@finn-thorben.me>
2022-04-28 09:06:53 +0200

    frontend: implement simple "logged in as…" badge

--- src/frontend/src/components/featureComponents/HeaderBar.vue
+++ src/frontend/src/components/featureComponents/HeaderBar.vue
@@ -1,7 +1,26 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import NameBadge from "@/components/featureComponents/NameBadge.vue";
+import { useUserStore } from "@/stores/userStore";
+
+const userStore = useUserStore();
+</script>
 
 <template>
-  <h2>Hello World</h2>
+  <div class="profile-badge">
+    <h4 v-if="userStore.me">Logged in as {{ userStore.me.username }}</h4>
+    <NameBadge :name="userStore.me?.username" />
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.profile-badge {
+  position: absolute;
+  top: 0;
+  right: 0;
+  margin: 12px;
+
+  display: flex;
+  align-items: center;
+  gap: 16px;
+}
+</style>
---
+++ src/frontend/src/components/featureComponents/NameBadge.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import Avatar from "vue-boring-avatars";
+
+defineProps<{
+  name: string;
+}>();
+</script>
+
+<template>
+  <Avatar class="avatar" :name="name" variant="beam" />
+</template>
+
+<style scoped>
+.avatar {
+  height: 64px;
+  width: 64px;
+}
+</style>
