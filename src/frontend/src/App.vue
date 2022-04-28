2c2fdd72a11131b52f8dffe91aba72f300b0b594
Finn-Thorben Sell <dev@finn-thorben.me>
2022-04-28 08:37:45 +0200

    frontend: remove automatic authentication on app startup

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,26 +1,6 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
-import { onMounted } from "vue";
-import { useRestApi } from "@/apiClient";
-import { useAuthStore } from "@/stores/authStore";
 import HeaderBar from "@/components/featureComponents/HeaderBar.vue";
-
-let api = useRestApi();
-let authStore = useAuthStore();
-
-onMounted(async () => {
-  if (!authStore.isAuthenticated) {
-    const response = await api.value.auth.login({
-      loginRequest: {
-        username: "ftsell",
-        password: "foobar123",
-      },
-    });
-
-    authStore.authToken = response.authToken;
-    authStore.persistAuth();
-  }
-});
 </script>
 
 <template>
